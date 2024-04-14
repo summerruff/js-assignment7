8db0d6619aa90fc12fb4b5403fc5c88d4051969d
Summer Ruff <summerruff13@gmail.com>
2024-04-13 19:12:46 -0500

    initial commit

--- app.js
+++ app.js
@@ -20,11 +20,29 @@ app.get('/', (_, response) => {
 
 
 // GET /api/todos
+app.get('/api/todos', (req, res) => {
+	res.json(todos)
+})
 
 // POST /api/todos
+app.post('/api/todos', (req, res) => {
+	const { item } = req.body
+	const id = todos.length + 1
+	const complete = false
+	todos.push({ id, item, complete })
+
+	res.json({ id })
+})
 
 // PUT /api/todos/:id
+app.put('/api/todos/:id', (req, res) => {
+	const { id } = req.params
+	const task = todos.find(todo => todo.id.toString() === id)
+	task.complete = !task.complete // toggle the complete property
 
+
+	res.json({ id, complete: task.complete })
+})
 
 
 const message = `Server running: http://localhost:${port}`
